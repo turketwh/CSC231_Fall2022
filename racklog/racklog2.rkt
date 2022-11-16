dc65a37caf3e1aba66ee409ed40dbc19a2130693
William Turkett <5298611+turketwh@users.noreply.github.com>
2022-11-15 19:00:27 -0500

    Update racklog2.rkt to include course relations

--- racklog/racklog2.rkt
+++ racklog/racklog2.rkt
@@ -108,3 +108,42 @@
          (%<= temp 65)
          (%>= windSpeed 10)]
         ))
+
+; courses taken relation
+(define %taken
+  (%rel ()
+        [('ben 'csc 111)]
+        [('ben 'csc 112)]
+        [('ben 'mth 111)]
+        [('ben 'mth 117)]
+        [('ben 'csc 201)]
+        [('ben 'csc 231)]
+        [('mary 'csc 111)]
+        [('mary 'csc 112)]
+        [('mary 'mth 112)]
+        [('mary 'mth 117)]
+        [('mary 'csc 201)]
+        [('mary 'csc 241)]
+        [('mary 'csc 321)]
+        [('jamie 'csc 111)]
+        [('jamie 'csc 112)]
+        [('jamie 'mth 117)]
+        [('jamie 'mth 121)]
+        [('jamie 'csc 201)]
+        [('jamie 'csc 191)]
+        [('jamie 'csc 391)]
+        [('carrie 'csc 111)]
+        [('carrie 'csc 112)]
+        [('carrie 'mth 111)]
+        [('carrie 'mth 112)]
+        [('carrie 'mth 205)]
+        ))
+
+; completedCalculus relation
+(define %completedCalculus
+  (%rel (Student)
+        [(Student)
+         (%taken Student 'mth 111)]
+        [(Student)
+         (%taken Student 'mth 112)]
+        ))
